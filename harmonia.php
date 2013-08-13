9d27561be24b3a1f064be0f2d98c7a5ec0b9b5af
Ned Zimmerman <ned@bight.ca>
2013-08-13 20:29:26 -0300

    Initial commit.

---
+++ harmonia.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package Harmonia
+ * @version 1.0
+ */
+/*
+Plugin Name: Harmonia
+Plugin URI: http://wordpress.org/plugins/harmonia/
+Description: Turns links to MP3 and M4A files into minimalist inline audio players.
+Author: Ned Zimmerman
+Version: 1.0
+Author URI: http://bight.ca/
+*/
+
+define( 'HARMONIA_VERSION', 1 );
+define( 'HARMONIA_URL', plugin_dir_url( __FILE__ ));
+
+add_action( 'wp_enqueue_scripts', 'harmonia_enqueue_assets' );
+
+function harmonia_enqueue_assets() { ?>
+	<script type="text/javascript">var flashPath = '<?php echo HARMONIA_URL; ?>swf/';</script>
+	<?php wp_enqueue_script( 'jquery' );
+	wp_enqueue_script( 'jplayer-js', HARMONIA_URL .'js/jplayer.min.js', 'jquery', '2.2.0' );
+	wp_enqueue_script( 'harmonia-js', HARMONIA_URL .'js/harmonia.js', 'jquery', HARMONIA_VERSION );
+	wp_enqueue_style(  'harmonia-css', HARMONIA_URL .'css/harmonia.css', null, HARMONIA_VERSION );
+}
---
+++ js/harmonia.js
@@ -0,0 +1,43 @@
+jQuery( document ).ready( function( $ ) {
+	$('a').each(function (i) {
+		if (this.href.match(/\.mp3$/i)||this.href.match(/\.m4a$/i)) {
+			var songID = i;
+			$(this).before('<div id="harmonia-player-'+songID+'" class="harmonia-player"></div><span id="harmonia-controller-'+songID+'" class="harmonia-controller"><a href="javascript:;" id="play"><i class="play"></i></a><a href="javascript:;" id="pause" style="display: none;"><i class="pause"></i></a></span>');
+		}
+	});
+	$('a#play').on("click", function( event ){
+		var songLink = $(this).parent('span').next('a').attr("href");
+		var songType = 'mp3';
+		if (songLink.match(/\.mp3$/i)) {
+			songType = 'mp3';
+		} else if (songLink.match(/\.m4a$/i)) {
+			songType = 'm4a';
+		}
+		var controllerAncestor = $(this).parent('span').attr("id");
+		$(this).parent('span').prev('div').jPlayer({
+			play: function( event ) { // To avoid both jPlayers playing together.
+				$(this).jPlayer("pauseOthers");
+			},
+			ready: function () {
+				if ( songType === 'mp3' ) {
+					$(this).jPlayer("setMedia", {
+						mp3:songLink
+					});
+				} else if ( songType === 'm4a' ) {
+					$(this).jPlayer("setMedia", {
+						m4a:songLink
+					});
+				}
+				$(this).jPlayer("play");
+			},
+			cssSelectorAncestor: '#'+controllerAncestor,
+			cssSelector: {
+				play: "#play",
+				pause: "#pause"
+			},
+			swfPath: flashPath,
+			solution:"html,flash",
+			supplied:songType
+		});
+	});
+} );
